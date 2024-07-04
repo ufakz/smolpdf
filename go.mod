04c0a6dd830749c9b7d0e7e15f6c74f0e1d22a1e
Farouq Umar <ufakz99@gmail.com>
2024-07-05 02:40:47 +0300

    feat: handler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ufakz/smolpdf
 
 go 1.21.3
+
+require github.com/pdfcpu/pdfcpu v0.8.0 // indirect
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,6 +1,14 @@
 package handler
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/model"
+	"github.com/ufakz/smolpdf/pkg/errors"
+)
 
 type Handler interface {
 	Merge(ctx context.Context, inputFiles []string, outputFile string) error
@@ -12,4 +20,33 @@ type PDFcpuHandler struct {
 
 func NewPDFHandler() *PDFcpuHandler {
 	return &PDFcpuHandler{}
+}
+
+func (h *PDFcpuHandler) Merge(ctx context.Context, inputFiles []string, outputFile string) error {
+	if len(inputFiles) < 2 {
+		return errors.ErrInsufficientInputFiles
+	}
+
+	conf := model.NewDefaultConfiguration()
+	err := api.MergeCreateFile(inputFiles, outputFile, false, conf)
+
+	if err != nil {
+		return fmt.Errorf("%w: %v", errors.ErrMergeFailed, err)
+	}
+
+	return nil
+}
+
+func (h *PDFcpuHandler) Split(ctx context.Context, inputFile string, outputDir string) error {
+	if err := os.MkdirAll(outputDir, 0755); err != nil {
+		return fmt.Errorf("%w: %v", errors.ErrCreateOutputDir, err)
+	}
+
+	conf := model.NewDefaultConfiguration()
+	err := api.SplitFile(inputFile, outputDir, 1, conf)
+	if err != nil {
+		return fmt.Errorf("%w: %v", errors.ErrSplitFailed, err)
+	}
+
+	return nil
 }
